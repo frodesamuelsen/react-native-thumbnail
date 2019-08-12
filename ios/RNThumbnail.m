f31fc00b94c50ebc2102dec89c62f9c4282ded5c
Nevo <nevo.lee@gmail.com>
2019-08-12 16:52:46 +0800

    bug fix

--- ios/RNThumbnail.m
+++ ios/RNThumbnail.m
@@ -53,14 +53,8 @@ RCT_EXPORT_METHOD(get:(NSString *)filepath resolve:(RCTPromiseResolveBlock)resol
         
         CGImageRef imgRef = [generator copyCGImageAtTime:time actualTime:NULL error:&err];
         UIImage *thumbnail = [UIImage imageWithCGImage:imgRef];
-        // save to temp directory
-        NSString* tempDirectory = [NSSearchPathForDirectoriesInDomains(NSCachesDirectory,
-                                                                       NSUserDomainMask,
-                                                                       YES) lastObject];
         
         NSData *data = UIImageJPEGRepresentation(thumbnail, 1.0);
-        NSFileManager *fileManager = [NSFileManager defaultManager];
-        NSString *fullPath = [tempDirectory stringByAppendingPathComponent: [NSString stringWithFormat:@"thumb-%@.jpg", [[NSProcessInfo processInfo] globallyUniqueString]]];
         [fileManager createFileAtPath:fullPath contents:data attributes:nil];
         CGImageRelease(imgRef);
         if (resolve)
