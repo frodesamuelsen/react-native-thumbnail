99e65c1de9a8ffcc05f47a4566262fe23feac6ef
Nevo <nevo.lee@gmail.com>
2019-08-12 11:44:12 +0800

    commit

--- android/src/main/java/me/hauvo/thumbnail/RNThumbnailModule.java
+++ android/src/main/java/me/hauvo/thumbnail/RNThumbnailModule.java
@@ -4,21 +4,23 @@ package me.hauvo.thumbnail;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.bridge.Callback;
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.WritableMap;
+import android.media.ThumbnailUtils;
+import android.provider.MediaStore;
+import android.provider.MediaStore.Video.Thumbnails;
 import android.graphics.Bitmap;
 import android.os.Environment;
 import android.util.Log;
 import android.media.MediaMetadataRetriever;
+import 	android.graphics.Matrix;
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import java.util.UUID;
 import java.io.File;
 import java.io.OutputStream;
 import java.io.FileOutputStream;
-import java.util.HashMap;
 
 
 public class RNThumbnailModule extends ReactContextBaseJavaModule {
@@ -58,60 +60,56 @@ public class RNThumbnailModule extends ReactContextBaseJavaModule {
       return hex.toString();
   }
 
-
   @ReactMethod
   public void get(String filePath, Promise promise) {
-  	try {
-	    filePath = filePath.replace("file://","");
-	    String fullPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/thumb";
-	    String fileName = "thumb-" + getMD5(filePath) + ".jpeg";
+    try {
+      filePath = filePath.replace("file://","");
+      String fullPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/thumb";
+      String fileName = "thumb-" + getMD5(filePath) + ".jpeg";
 
-	    File cache = new File(fullPath, fileName);
-	    if (cache.exists()) {
-	      WritableMap map = Arguments.createMap();
-	      map.putString("path", "file://" + fullPath + '/' + fileName);
-	      promise.resolve(map);
-	      return;
-	    }
+      File dir = new File(fullPath);
+      if (!dir.exists()) {
+        dir.mkdirs();
+      }
 
-	    MediaMetadataRetriever retriever = new MediaMetadataRetriever();
-	    if (filePath.startsWith("http")) {
-			retriever.setDataSource(filePath, new HashMap<String, String>());
-		} else {
-			retriever.setDataSource(filePath);
-		}
-	    Bitmap image = retriever.getFrameAtTime(1000000, MediaMetadataRetriever.OPTION_CLOSEST);
+      File cache = new File(fullPath, fileName);
+      if (cache.exists()) {
+        WritableMap map = Arguments.createMap();
+        map.putString("path", "file://" + fullPath + '/' + fileName);
+        promise.resolve(map);
+        return;
+      }
 
-		File dir = new File(fullPath);
-		if (!dir.exists()) {
-			dir.mkdirs();
-		}
+      MediaMetadataRetriever retriever = new MediaMetadataRetriever();
+      if (filePath.startsWith("http")) {
+        retriever.setDataSource(filePath, new HashMap<String, String>());
+      } else {
+        retriever.setDataSource(filePath);
+      }
+      Bitmap image = retriever.getFrameAtTime(1000000, MediaMetadataRetriever.OPTION_CLOSEST_SYNC);
+    
+      OutputStream fOut = null;
+      File file = new File(fullPath, fileName);
+      file.createNewFile();
+      fOut = new FileOutputStream(file);
 
-		OutputStream fOut = null;
-		// String fileName = "thumb-" + UUID.randomUUID().toString() + ".jpeg";
+      // 100 means no compression, the lower you go, the stronger the compression
+      image.compress(Bitmap.CompressFormat.JPEG, 60, fOut);
+      fOut.flush();
+      fOut.close();
 
-		File file = new File(fullPath, fileName);
-		file.createNewFile();
-		fOut = new FileOutputStream(file);
+      // MediaStore.Images.Media.insertImage(reactContext.getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName());
 
-		// 100 means no compression, the lower you go, the stronger the compression
-		image.compress(Bitmap.CompressFormat.JPEG, 60, fOut);
-		fOut.flush();
-		fOut.close();
+      WritableMap map = Arguments.createMap();
 
-		// MediaStore.Images.Media.insertImage(reactContext.getContentResolver(), file.getAbsolutePath(), file.getName(), file.getName());
+      map.putString("path", "file://" + fullPath + '/' + fileName);
+      map.putDouble("width", image.getWidth());
+      map.putDouble("height", image.getHeight());
 
-		WritableMap map = Arguments.createMap();
+      promise.resolve(map);
 
-		map.putString("path", "file://" + fullPath + '/' + fileName);
-		map.putDouble("width", image.getWidth());
-		map.putDouble("height", image.getHeight());
-
-		promise.resolve(map);
-
-		} catch (Exception e) {
-			//Log.e("E_RNThumnail_ERROR", e.getMessage());
-			promise.reject("E_RNThumnail_ERROR", e);
-		}
+    } catch (Exception e) {
+      promise.reject("E_RNThumnail_ERROR", e);
     }
+  }
 }
